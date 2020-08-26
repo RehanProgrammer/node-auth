fdb156b754ad406a861b3c9ac2b8f5b89c81ad6c
Rehan Ayub <Piece-of-Shit@Mohammeds-MacBook-Pro-2.local>
2020-08-25 23:19:37 -0500

    iinserted data to db

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,23 @@
+const User = require('../models/User');
+
 module.exports.signUp_get = (req,res) =>{
     res.render('signup');
 }
 
 module.exports.signUp_post = (req,res) =>{
-    res.send('new signup');
+    const { email, password } = req.body;
+
+    
+        User
+        .create({email,password})
+        .then(user =>{
+            console.log(user);
+            res.status(201).json(user)
+        })
+        .catch(error=>{
+        console.log("Rehan",error);
+        res.status(400).send('error, user not created');
+    });
 }
 
 module.exports.login_get = (req,res) =>{
@@ -11,6 +25,6 @@ module.exports.login_get = (req,res) =>{
 }
 
 module.exports.login_post = (req,res) =>{
-    const { emai }
+    const { email, password } = req.body;
     res.send('user login');
 }
