c28f25491d2c48be43fd91120b21664a0a91d818
Rehan <ayoubrehan@gmail.com>
2020-09-13 16:43:38 -0500

    implemented jwt auth and error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,48 +1,54 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const handleErrors = (err) => {
-    let error = {email:'',password:''};
+  let error = { email: "", password: "" };
 
-    //duplicate email found
-    if (err.code === 11000){
-        error.email = 'that email is already in use';
-        return error;
-    }
-
-    if (err.message.includes('user validation failed')){
-        Object.values(err.errors).forEach(({properties}) => {
-            error[properties.path] = properties.message;
-        })
-        //console.log(err);
-    }
+  //duplicate email found
+  if (err.code === 11000) {
+    error.email = "that email is already in use";
     return error;
-}
+  }
 
-module.exports.signUp_get = (req,res) =>{
-    res.render('signup');
-}
-
-module.exports.signUp_post = (req,res) =>{
-    const { email, password } = req.body;
-    
-    User
-        .create({email,password})
-        .then(user =>{
-            console.log(user);
-            res.status(201).json(user)
-        })
-        .catch(error=>{
-        const errors =handleErrors(error);
-        console.log('error: ',error);
-        res.status(400).send(errors);
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      error[properties.path] = properties.message;
     });
-}
+    //console.log(err);
+  }
+  return error;
+};
+const maxAge = 3 * 24 * 60 * 60; //in seconds
+const createToken = (id) => {
+  return jwt.sign({ id }, "Rehan secret", {
+    expiresIn: maxAge,
+  });
+};
 
-module.exports.login_get = (req,res) =>{
-    res.render('login');
-}
+module.exports.signUp_get = (req, res) => {
+  res.render("signup");
+};
 
-module.exports.login_post = (req,res) =>{
-    const { email, password } = req.body;
-    res.send('user login');
-}
+module.exports.signUp_post = (req, res) => {
+  const { email, password } = req.body;
+
+  User.create({ email, password })
+    .then((user) => {
+      const token = createToken(user._id);
+      res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+      res.status(201).send({ user: user._id });
+    })
+    .catch((error) => {
+      const errors = handleErrors(error);
+      res.status(400).send(errors);
+    });
+};
+
+module.exports.login_get = (req, res) => {
+  res.render("login");
+};
+
+module.exports.login_post = (req, res) => {
+  const { email, password } = req.body;
+  res.send("user login");
+};
--- models/User.js
+++ models/User.js
@@ -1,42 +1,37 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
-
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true,'please enter email'],
-        unique: true,
-        lowercase:true,
-        validate: [isEmail,"Enter valid email"]
-    },
-    password:{
-        type: String,
-        required: [true,'please enter password'],
-        minlength: [6,'Min pass length is 6 char']
-    },
+  email: {
+    type: String,
+    required: [true, "please enter email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Enter valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter password"],
+    minlength: [6, "Min pass length is 6 char"],
+  },
 });
 
 //fire function after document saved in database
-userSchema.post('save', (doc, next) => {
-    
-    console.log('x from arrow func: ',this);
-    console.log('new user was created & saved', doc);
-
-    next()
-})
+userSchema.post("save", (doc, next) => {
+  next();
+});
 
 //fire a function before a doc was saved into db
-userSchema.pre('save', async function (next){
-    const salt = await bcrypt.genSalt();
-    console.log('user about to be saved in DB', this);
-    //"this" refers to the "User" object in normal function. theirfore we have have access to email and password property
-    this.password = await bcrypt.hash(this.password,salt);
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
 
-    next();
-})
+  //"this" refers to the "User" object in normal function. theirfore we have have access to email and password property
+  this.password = await bcrypt.hash(this.password, salt);
 
-const User = mongoose.model('user', userSchema);
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
